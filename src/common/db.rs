fedeb26389bcbf5fd5798020974981cadd138b16
uuip <uuip@users.noreply.github.com>
2023-05-25 14:34:42 +0800

    set pool size

--- src/common/db.rs
+++ src/common/db.rs
@@ -9,7 +9,7 @@ pub struct AppState {
 pub async fn connection() -> PgPool {
     let dsn = dotenvy::var("DATABASE_URL").unwrap();
     PgPoolOptions::new()
-        .max_connections(800)
+        .max_connections(3000)
         .min_connections(64)
         .connect(&dsn)
         .await
